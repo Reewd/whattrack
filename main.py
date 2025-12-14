c5d8c8a89934e7cb67105df1e8eb37f670109848
Francesco De Benedittis <contact@francescodb.it>
2025-12-14 11:29:53 +0100

    Added argparser for training hyperparameters

--- main.py
+++ main.py
@@ -7,11 +7,22 @@ import lightning as L
 from pytorch_lightning.loggers import WandbLogger
 from lightning.pytorch.tuner import Tuner
 from lightning.pytorch.callbacks import ModelCheckpoint, DeviceStatsMonitor
-
+import argparse
 def main():
-    train_path = "dataset/music/train-10k-30s"
-    val_path = "dataset/music/val-query-db-500-30s"
-    test_path = "dataset/music/test-query-db-500-30s/db"
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument('--resume_from_checkpoint', type=str, default=None, help='Path to checkpoint to resume from')
+    argparser.add_argument('--train-path', type=str, default='dataset/music/train-10k-30s', help='Path to training dataset')
+    argparser.add_argument('--val-path', type=str, default='dataset/music/val-query-db-500-30s', help='Path to validation dataset')
+    argparser.add_argument('--test-path', type=str, default='dataset/music/test-query-db-500-30s/db', help='Path to test dataset')  
+    argparser.add_argument('--aug-dir', type=str, default='dataset/aug', help='Path to augmentation directory')
+    argparser.add_argument('--batch-size', type=int, default=300, help='Batch size for training')
+    argparser.add_argument('--num-workers', type=int, default=32, help='Number of workers for data loading')
+    argparser.add_argument('--max-epochs', type=int, default=5, help='Maximum number of training epochs')
+    args = argparser.parse_args()
+
+    train_path = args.train_path
+    val_path = args.val_path
+    test_path = args.test_path
     aug_dir = "dataset/aug"
     wandb_logger = WandbLogger(project="FDS")
     model = LitContrastive()
@@ -32,7 +43,7 @@ def main():
     ]
 )
 
-    dm = AudioDataModule(train_path=train_path, val_path=val_path, test_path=test_path, num_workers=32, train_augmentations=augmentations, batch_size=300)
+    dm = AudioDataModule(train_path=train_path, val_path=val_path, test_path=test_path, num_workers=args.num_workers, train_augmentations=augmentations, batch_size=args.batch_size)
     
     # Configure checkpoint callback to save best model based on lowest loss
     checkpoint_callback = ModelCheckpoint(
@@ -47,7 +58,7 @@ def main():
     # Monitor GPU usage metrics
     device_stats = DeviceStatsMonitor()
     
-    trainer = L.Trainer(max_epochs=5, logger=wandb_logger, callbacks=[checkpoint_callback, device_stats])
+    trainer = L.Trainer(max_epochs=args.max_epochs, logger=wandb_logger, callbacks=[checkpoint_callback, device_stats])
     dm.setup()
     trainer.fit(model=model, train_dataloaders=dm.train_dataloader(), val_dataloaders=dm.val_dataloader())
     
